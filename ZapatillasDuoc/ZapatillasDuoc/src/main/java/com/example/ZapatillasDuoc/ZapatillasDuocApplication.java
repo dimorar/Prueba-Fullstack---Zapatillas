768389b6cfe480401bdec123e6ed6a15d2666044
dimorar <di.morar@duocuc.cl>
2026-05-02 11:37:07 -0400

    Add files via upload

---
+++ ZapatillasDuoc/ZapatillasDuoc/src/main/java/com/example/ZapatillasDuoc/ZapatillasDuocApplication.java
@@ -0,0 +1,13 @@
+package com.example.ZapatillasDuoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZapatillasDuocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZapatillasDuocApplication.class, args);
+	}
+
+}
---
+++ ZapatillasDuoc/ZapatillasDuoc/src/test/java/com/example/ZapatillasDuoc/ZapatillasDuocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ZapatillasDuoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZapatillasDuocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
