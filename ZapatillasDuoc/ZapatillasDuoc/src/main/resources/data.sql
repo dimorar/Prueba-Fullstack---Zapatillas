c55df5ad437b56ccbc308db09209a4b4f02bee66
dimorar <di.morar@duocuc.cl>
2026-05-03 15:26:24 -0400

    Create data.sql

---
+++ ZapatillasDuoc/ZapatillasDuoc/src/main/resources/data.sql
@@ -0,0 +1,23 @@
+INSERT INTO zapatilla (modelo, codigo, genero, precio, descripcion, stock) VALUES
+('R4 Black',             'r4black',      'UNISEX', 89990,  'Zapatilla clasica estilo running colorway negro total. Disponible para hombre y mujer.', 50),
+('TN Negras',            'tn-negras',    'HOMBRE', 109990, 'Nike Air Max TN colorway negro. Icono del estilo urbano masculino.', 30),
+('R11 Cool Grey',        'r11-cool-grey','HOMBRE', 99990,  'Nike R11 colorway gris cool grey. Diseno moderno y comodo para hombre.', 25),
+('Air Force One Blancas','af1-blancas',  'MUJER',  94990,  'Nike Air Force 1 Low blanco total. Clasico atemporal exclusivo para mujer.', 40),
+('Nike Shox TL',         'shox-tl',      'MUJER',  119990, 'Nike Shox TL con tecnologia de columnas Shox. Exclusivo para mujer.', 20);
+
+INSERT INTO talla (numero, zapatilla_id) VALUES
+(36.0,1),(36.5,1),(37.0,1),(37.5,1),(38.0,1),(38.5,1),(39.0,1),
+(40.0,1),(40.5,1),(41.0,1),(41.5,1),(42.0,1),(42.5,1),(43.0,1),(43.5,1),(44.0,1);
+
+INSERT INTO talla (numero, zapatilla_id) VALUES
+(40.0,2),(40.5,2),(41.0,2),(41.5,2),(42.0,2),(42.5,2),(43.0,2),(43.5,2),(44.0,2);
+
+INSERT INTO talla (numero, zapatilla_id) VALUES
+(40.0,3),(40.5,3),(41.0,3),(41.5,3),(42.0,3),(42.5,3),(43.0,3),(43.5,3),(44.0,3);
+
+INSERT INTO talla (numero, zapatilla_id) VALUES
+(36.0,4),(36.5,4),(37.0,4),(37.5,4),(38.0,4),(38.5,4),(39.0,4);
+
+INSERT INTO talla (numero, zapatilla_id) VALUES
+(36.0,5),(36.5,5),(37.0,5),(37.5,5),(38.0,5),(38.5,5),(39.0,5);
+
